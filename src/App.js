d8075f7648ff703759f18ead48cb102f4eeccc98
Miron Kucher <Mirverch@yandex.ru>
2022-03-17 18:05:42 +0500

    fix(app): remove logs

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ const App = () => {
       <h1>Раскрывающиеся объекты</h1>
       {keysArr.map(value => {
         if(typeof obj[value] === 'object' && typeof obj[value] !== null) {
-          console.log('obj[value] - ', obj[value])
           return <Item key={value} obj={obj[value]} header={value}/>
         } else {
           return (
